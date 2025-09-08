b24da5fafe244e5e82e0e06e3010d97928530c42
andietiag <andietiag@gmail.com>
2025-09-08 18:53:16 -0400

    Added search functionality using API

--- src/script.js
+++ src/script.js
@@ -1,9 +1,25 @@
+function newWeather(response) {
+    let temperatureElement = document.querySelector("#current-temperature-value");
+    let temperature = response.data.temperature.current;
+    let newTitle = document.querySelector("#city");
+    newTitle.innerHTML = `${response.data.city}`;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function newCity(city) {
+    let apiKey = "4cfo2b293cf1a7894a8e2380489tdbd3";
+    let units = "metric"
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(newWeather);
+}
+
 function newSearch(event) {
     event.preventDefault();
     let searchedCity = document.querySelector("#input");
-    let newCity = document.querySelector("#city");
-    newCity.innerHTML = `${searchedCity.value}`;
+    newCity(searchedCity.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", newSearch);
+
+newCity("Hermosillo");
