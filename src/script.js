8ff8acd921a650ed08e2c2dd3851a5f0f2c00b67
andietiag <andietiag@gmail.com>
2025-09-08 18:10:40 -0400

    Add basic search engine

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
+function newSearch(event) {
+    event.preventDefault();
+    let searchedCity = document.querySelector("#input");
+    let newCity = document.querySelector("#city");
+    newCity.innerHTML = `${searchedCity.value}`;
+}
+
 let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", newSearch);
